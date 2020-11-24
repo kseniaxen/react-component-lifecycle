8931d06d9579c2bd15f3009b385a7ff883853404
YuriiTrofimenko <tyaa@ukr.net>
2020-11-24 11:47:44 +0200

    @material-ui/styles for class styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from "./components/Home"
 import About from "./components/About"
-import {Navbar} from "./components/base/Navbar"
+import Navbar from "./components/base/Navbar"
 import RouteModel from "./models/RouteModel";
 
 const routes: RouteModel[] = [
-    new RouteModel("/", Home, 'Home'),
+    /*new RouteModel("/", Home, 'Home'),*/
     new RouteModel("/about", About, 'About')
 ]
 function App() {
@@ -16,6 +16,10 @@ function App() {
         <Navbar routes={routes}/>
         {/*<Navbar/>*/}
         <Switch>
+            <Route
+                exact
+                path='/'
+                component={Home}/>
             {
                 routes.map(
                     routeModel =>
--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -1,32 +1,77 @@
 import React from 'react'
-import {Link, Route} from "react-router-dom"
-import RouteModel from "../../models/RouteModel";
+import {Link, NavLink, Route} from "react-router-dom"
+import {makeStyles, WithStyles, withStyles} from '@material-ui/styles'
+import RouteModel from "../../models/RouteModel"
 
-interface IProps {
+interface IProps extends WithStyles<typeof styles> {
     routes: RouteModel[]
 }
 
 interface IState {
 }
 
-export class Navbar extends React.Component<IProps, IState> {
+const styles = () => ({
+    links: {
+        listStyleType: 'none',
+        margin: '0',
+        padding: '0',
+        overflow: 'hidden',
+        backgroundColor: '#333',
+        position: 'sticky' as 'sticky',
+        top: '0',
+    },
+    link: {
+        float: 'left' as 'left',
+        '& a': {
+            display: 'block',
+            color: 'white',
+            textAlign: 'center',
+            padding: '14px 16px',
+            textDecoration: 'none',
+        },
+        '& a:hover': {
+            backgroundColor: '#111'
+        },
+        '& .active': {
+            backgroundColor: '#4CAF50'
+        },
+    },
+})
+
+class Navbar extends React.Component<IProps, IState> {
     constructor(props: IProps | Readonly<IProps>) {
         super(props)
     }
   render() {
       const routes = this.props.routes
+      const { classes } = this.props
       return (
-          <nav>
+          <ul className={classes.links}>
+              <li className={classes.link}>
+                  <NavLink
+                      to='/'
+                      activeClassName='active'
+                      exact={true}
+                  >
+                      Home
+                  </NavLink>
+              </li>
               {
                   routes.map(
                       routeModel =>
-                          <Link
-                              to={routeModel.path}>
-                              {routeModel.name}
-                          </Link>
+                          <li className={classes.link}>
+                              <NavLink
+                                  to={routeModel.path}
+                                  activeClassName='active'
+                              >
+                                  {routeModel.name}
+                              </NavLink>
+                          </li>
                   )
               }
-          </nav>
+          </ul>
       )
   }
 }
+
+export default withStyles(styles)(Navbar)
